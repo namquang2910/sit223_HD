f4ab4bffeb619963ef669a403b879776abf75579
Van Nam Quang Nguyen <s223149341@deakin.edu.au>
2024-09-22 09:01:06 +1000

    typing animation fix

--- script/start_server.sh
+++ script/start_server.sh
@@ -5,7 +5,5 @@ cd /var/www/myapp
 
 # Start your server (adjust this command based on your setup)
 # For example, using Node.js:
-node start server.js  # or any command you use to run your application
+npm start
 
-# Ensure that the server is started properly
-pm2 save
