007a4d7c511ac04ebaf9140184eadb3c76c11365
namquang2910 <139729395+namquang2910@users.noreply.github.com>
2024-09-22 14:27:20 +1000

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,7 +3,5 @@
 # Navigate to the application directory
 cd /var/www/myapp
 
-# Start your server (adjust this command based on your setup)
-# For example, using Node.js:
-npm start
+pm2 start http-server -- -p 8080 dist
 
