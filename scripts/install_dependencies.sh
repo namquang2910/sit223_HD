90913ce91bd2ce8d593de555e91c9c628505263b
namquang2910 <139729395+namquang2910@users.noreply.github.com>
2024-09-22 11:48:02 +1000

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,13 @@
 # Update package lists for Amazon Linux
-sudo yum install -y nodejs 
+sudo yum update -y
 
+sudo yum install -y nodejs npm
+
+# Create the deployment directory if it doesn't exist
+if [ ! -d "/var/www/myapp" ]; then
+  sudo mkdir -p /var/www/myapp
+fi
+# Navigate to the deployment directory
+cd /var/www/myapp
 # Install project dependencies
 npm install
