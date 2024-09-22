a131777c8999ccb8bff653dd706c2ba5dbb689bf
namquang2910 <139729395+namquang2910@users.noreply.github.com>
2024-09-22 14:29:34 +1000

    Update check_status.sh

--- scripts/check_status.sh
+++ scripts/check_status.sh
@@ -3,6 +3,7 @@
 # Check if the server is running
 if pm2 status | grep -q 'online'; then
   echo "Application is running."
+  exit 0
 else
   echo "Application failed to start."
   exit 1  # Exit with a non-zero status to signal failure
