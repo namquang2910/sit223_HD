e6315ffe4e4db0a1fc56cfcb71e222ffc2881548
Van Nam Quang Nguyen <s223149341@deakin.edu.au>
2024-09-24 06:06:59 +1000

    server change

--- server.js
+++ server.js
@@ -1,40 +1,24 @@
 const express = require('express');
 const path = require('path');
-const StatsD = require('hot-shots');
-
+const winston = require('winston');
 const app = express();
-const dogstatsd = new StatsD({
-  host: 'us5.datadoghq.com', // Use 'us5.datadoghq.com' for Datadog's US region
-  port: 8125,
-  prefix: 'simpleweb' // Replace with your application's prefix
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+  transports: [
+    new winston.transports.Console(), // Send logs to the console (stdout)
+  ]
 });
 
-// Middleware to log requests
-app.use((req, res, next) => {
-  const startTime = Date.now(); // Start time to calculate response time
-
-  // Increment request count
-  dogstatsd.increment('request.count');
-
-  // End response and calculate duration
-  res.on('finish', () => {
-    const duration = Date.now() - startTime;
-    dogstatsd.timing('request.response_time', duration); // Log response time
-
-    // Increment response metrics based on status code
-    dogstatsd.increment('response.count'); // Total responses
-    if (res.statusCode >= 200 && res.statusCode < 300) {
-      dogstatsd.increment('response.success'); // Successful responses
-    } else if (res.statusCode >= 400 && res.statusCode < 500) {
-      dogstatsd.increment('response.client_errors'); // Client errors
-    } else if (res.statusCode >= 500) {
-      dogstatsd.increment('response.server_errors'); // Server errors
-    }
-  });
-
-  next();
+// Example route that logs information
+app.get('/', (req, res) => {
+  logger.info('Hello from Heroku NodeJS app!', { timestamp: new Date().toISOString() });
+  res.send('Logging with Winston and sending to Datadog!');
 });
-
 // Serve static files from the dist directory
 app.use(express.static(path.join(__dirname, 'dist')));
 
