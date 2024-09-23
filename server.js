b340da82e313811968a3e64281d7b009376e131f
Van Nam Quang Nguyen <s223149341@deakin.edu.au>
2024-09-24 09:03:31 +1000

    update interval

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Initialize the StatsD client
 const dogstatsd = new StatsD({
-  host: 'localhost', // Use the Datadog server (see below)
+  host: 'localhost', // Use the Datadog server
   port: 8125,
   prefix: 'myapp.' // Use a prefix for your application's metrics
 });
@@ -20,11 +20,28 @@ app.use((req, res, next) => {
 
   res.on('finish', () => {
     const duration = Date.now() - startTime;
-    dogstatsd.timing('request.response_time', duration); // Log request duration
+    // Log request duration
+    dogstatsd.timing('request.response_time', duration);
+    // Log response size
+    dogstatsd.histogram('response.size', res.get('Content-Length') || 0);
+    // Track HTTP status codes
+    dogstatsd.increment(`response.status.${res.statusCode}`);
+    dogstatsd.timing('response.average_time', duration);
+    // Increment response metrics based on status code
+    dogstatsd.increment('response.count'); // Total responses
+    if (res.statusCode >= 200 && res.statusCode < 300) {
+      dogstatsd.increment('response.success'); // Successful responses
+    } else if (res.statusCode >= 400 && res.statusCode < 500) {
+      dogstatsd.increment('response.client_errors'); // Client errors
+    } else if (res.statusCode >= 500) {
+      dogstatsd.increment('response.server_errors'); // Server errors
+    } 
   });
 
   next();
 });
+
+
 // Serve static files from the dist directory
 app.use(express.static(path.join(__dirname, 'dist')));
 
