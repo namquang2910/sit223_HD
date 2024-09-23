cea12b28f7cee87557e5865d5485837cacf01380
namquang2910 <139729395+namquang2910@users.noreply.github.com>
2024-09-23 17:41:16 +1000

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,7 +3,7 @@ export HOME=/home/ec2-user  # Change this to the actual user’s home directory
 
 # Navigate to the application directory
 cd /var/www/myapp
-
+pm2 kill
 pm2 start npm --name "deloy" -- start
 pm2 status
 exit 1
