34726d1a6e32311db6152f274a35c3ebf25a6e4f
Van Nam Quang Nguyen <s223149341@deakin.edu.au>
2024-09-24 08:17:53 +1000

    update interval

--- server.js
+++ server.js
@@ -1,72 +1,28 @@
 const express = require('express');
 const path = require('path');
-const winston = require('winston');
-const axios = require('axios');
+const StatsD = require('hot-shots');
 
 const app = express();
 
-// Datadog API configuration
-const DATADOG_API_KEY = process.env.DD_API_KEY; // Set your Datadog API key in environment variables
-const DATADOG_API_URL = 'https://api.datadoghq.com/api/v1/series?api_key=' + DATADOG_API_KEY;
-
-// Create a custom transport for sending metrics
-class DatadogTransport {
-  log(info, callback) {
-    setImmediate(() => this.emit('logged', info));
-
-    if (info.level === 'info') {
-      this.sendMetric('request.count', 1);
-    } else if (info.level === 'timing') {
-      this.sendMetric('request.response_time', info.duration);
-    }
-
-    callback();
-  }
-
-  sendMetric(metricName, value) {
-    const data = {
-      series: [{
-        metric: metricName,
-        points: [[Math.floor(Date.now() / 1000), value]],
-        type: 'gauge',
-        host: 'your-app-name.herokuapp.com', // Use your Heroku app name
-        tags: ['environment:production'] // You can add additional tags here
-      }]
-    };
-
-    // Send the metric to Datadog
-    axios.post(DATADOG_API_URL, data)
-      .then(response => {
-        console.log('Metric sent to Datadog:', response.data);
-      })
-      .catch(error => {
-        console.error('Error sending metric to Datadog:', error);
-      });
-  }
-}
-
-// Configure Winston logger with the custom transport
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  transports: [
-    new DatadogTransport(), // Add your custom transport
-    new winston.transports.Console(), // Log to console
-  ]
+// Initialize the StatsD client
+const dogstatsd = new StatsD({
+  host: 'datadog-server', // Use the Datadog server (see below)
+  port: 8125,
+  prefix: 'myapp.' // Use a prefix for your application's metrics
 });
 
-// Middleware to log requests and send metrics
+// Middleware to log request count and duration
 app.use((req, res, next) => {
   const startTime = Date.now();
+
+  // Increment request count
+  dogstatsd.increment('request.count');
+
   res.on('finish', () => {
     const duration = Date.now() - startTime;
-
-    // Log the request
-    logger.info(`Request to ${req.url} took ${duration}ms`, { duration });
-
-    // Send timing metric
-    logger.log({ level: 'timing', duration });
+    dogstatsd.timing('request.response_time', duration); // Log request duration
   });
+
   next();
 });
 
